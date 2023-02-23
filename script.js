e692329492e59ba2568e71b3d5fe038ed90be6f8
Jarryd <Jarryd.Florentino@gmail.com>
2023-02-23 20:21:45 +0100

    added game logic

--- script.js
+++ script.js
@@ -1,5 +1,34 @@
 'use strict';
 
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let currentScore = 20;
+
 document.querySelector('.check').addEventListener('click', function () {
-  console.log(document.querySelector('.guess').value);
+  const guess = Number(document.querySelector('.guess').value);
+
+  console.log(guess, typeof guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = '⛔ No Number!';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = '🎉 Correct Number!';
+  } else if (guess > secretNumber) {
+    if (currentScore > 1) {
+      document.querySelector('.message').textContent = '📈 Too High!';
+      currentScore--;
+      document.querySelector('.score').textContent = currentScore;
+    } else {
+      document.querySelector('.message').textContent = '💥 You lost the game!';
+      document.querySelector('.score').textContent = 0;
+    }
+  } else if (guess < secretNumber) {
+    if (currentScore > 1) {
+      document.querySelector('.message').textContent = '📉 Too Low!';
+      currentScore--;
+      document.querySelector('.score').textContent = currentScore;
+    } else {
+      document.querySelector('.message').textContent = '💥 You lost the game!';
+      document.querySelector('.score').textContent = 0;
+    }
+  }
 });
