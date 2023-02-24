6fcd463b754ec1eccd28c04a7ddf55f9d7355f5f
Jarryd <Jarryd.Florentino@gmail.com>
2023-02-24 13:12:19 +0100

    Added CSS manipulation and Again btn functionality

--- script.js
+++ script.js
@@ -1,34 +1,55 @@
 'use strict';
 
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let currentScore = 20;
 
+document.querySelector('.again').addEventListener('click', function () {
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.check').style.visibility = 'visible';
+  document.querySelector('.score').textContent = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  currentScore = 20;
+});
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
-
-  console.log(guess, typeof guess);
-
+  //No number typed
   if (!guess) {
     document.querySelector('.message').textContent = '⛔ No Number!';
+    //Correct guess
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = '🎉 Correct Number!';
+    document.querySelector('.number').textContent = secretNumber;
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.check').style.visibility = 'hidden';
+    document.querySelector('.number').style.width = '30rem';
+    while (document.querySelector('.highscore').textContent < currentScore) {
+      document.querySelector('.highscore').textContent = currentScore;
+    }
+    //Guess is too high
   } else if (guess > secretNumber) {
     if (currentScore > 1) {
       document.querySelector('.message').textContent = '📈 Too High!';
       currentScore--;
       document.querySelector('.score').textContent = currentScore;
     } else {
+      document.querySelector('body').style.backgroundColor = '#f01e2c';
       document.querySelector('.message').textContent = '💥 You lost the game!';
       document.querySelector('.score').textContent = 0;
+      document.querySelector('.check').style.display = 'none';
     }
+    //Guess is too low
   } else if (guess < secretNumber) {
     if (currentScore > 1) {
       document.querySelector('.message').textContent = '📉 Too Low!';
       currentScore--;
       document.querySelector('.score').textContent = currentScore;
     } else {
+      document.querySelector('body').style.backgroundColor = '#f01e2c';
       document.querySelector('.message').textContent = '💥 You lost the game!';
       document.querySelector('.score').textContent = 0;
+      document.querySelector('.check').style.display = 'none';
     }
   }
 });
